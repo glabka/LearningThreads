ee01d076f730ce924f953a5e363ff21f8983b447
glabka <glabka333@gmail.com>
2022-08-14 23:34:08 +0200

    My example task for synchronization of several thread working in several logical stages.

---
+++ src/producer_processor_saver/MainMultiple.java
@@ -0,0 +1,53 @@
+package producer_processor_saver;
+
+import java.util.Timer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MainMultiple {
+
+    public static void main(String[] args) {
+        LogicMultiple logic = new LogicMultiple(30, 5, 5, 500, 100, 1000);
+
+        ExecutorService executor = Executors.newFixedThreadPool(9);
+
+        long start = System.nanoTime();
+
+        for (int i = 0; i < 3; i++) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    logic.loadAll();
+                }
+            });
+
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    logic.processAll();
+                }
+            });
+
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    logic.storeAll();
+                }
+            });
+        }
+
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            long end = System.nanoTime();
+            System.out.println("execution time: " + ((end - start) / 1E6) + " ms");
+        }
+
+        System.out.println("program ended.");
+    }
+}
